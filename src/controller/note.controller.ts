44be8fed3988bb090048bb15ed243023d154b56d
Amodhx <amxdhnanditha@gmail.com>
2025-03-16 12:33:41 +0530

    created controller classes

---
+++ src/controller/note.controller.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+
+class NoteController{
+
+    async saveNoteData(req:Request,resp:Response){
+
+    }
+    async updateNoteData(req:Request,resp:Response){
+
+    }
+    async deleteNoteData(req:Request,resp:Response){
+
+    }
+    async getAllNoteData(req:Request,resp:Response){
+
+    }
+}
+const NoteControll = new NoteController();
+export default NoteControll;
---
+++ src/controller/task.controller.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express";
+
+class TaskController{
+
+    async saveTaskData(req:Request,resp:Response){
+
+    }
+    async updateTaskData(req:Request,resp:Response){
+
+    }
+    async deleteTaskData(req:Request,resp:Response){
+
+    }
+    async getAllTaskData(req:Request,resp:Response){
+        
+    }
+
+}
+const TaskControll = new TaskController();
+export default TaskControll;
