8a0f17cb9d5cf8a108a025ddb4cbf0bbac2a5976
Amodhx <amxdhnanditha@gmail.com>
2025-03-16 19:58:22 +0530

    task service done

--- src/service/task.service.ts
+++ src/service/task.service.ts
@@ -1,17 +1,34 @@
+import TaskRepository from "../dao/task.dao";
 import TaskDTO from "../dto/task.dto";
 
 class TaskService{
     async saveTask(taskData:TaskDTO){
-
+        try{
+            return await TaskRepository.save(taskData);
+        }catch(err){
+            throw err;
+        }
     }
     async updateTask(taskData:TaskDTO){
-
+        try{
+            return await TaskRepository.update(taskData);
+        }catch(err){
+            throw err;
+        }
     }
     async deleteTask(task_id:String){
-
+        try{
+            return await TaskRepository.delete(task_id);
+        }catch(err){
+            throw err;
+        }
     }
     async getAllTasks(){
-        
+        try{
+            return await TaskRepository.getAll();
+        }catch(err){
+            throw err;
+        }
     }
 }
 const TaskServices = new TaskService();
